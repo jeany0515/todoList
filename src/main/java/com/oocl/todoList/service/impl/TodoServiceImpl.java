f2d0abe56ac05e50b761e371e28fc09f5b166a05
alex <alex.h.z.xu@oocl.com>
2020-08-05 18:46:10 +0800

    [alex xu] feature: add method to add todo

--- src/main/java/com/oocl/todoList/service/impl/TodoServiceImpl.java
+++ src/main/java/com/oocl/todoList/service/impl/TodoServiceImpl.java
@@ -48,7 +48,7 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public Todo addTodo(Todo todo) {
-        return null;
+        return todoRepository.save(todo);
     }
 
     public boolean isTodoExisted(Integer id) {
