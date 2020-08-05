ec5e1d2900fdc293b4e7c3af10522aa5e0f4fc50
alex <alex.h.z.xu@oocl.com>
2020-08-05 19:15:22 +0800

    [alex xu] feature: add api to return list of todos

---
+++ src/main/java/com/oocl/todoList/controller/TodoController.java
@@ -0,0 +1,29 @@
+package com.oocl.todoList.controller;
+
+import com.oocl.todoList.entity.Todo;
+import com.oocl.todoList.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author XUAL7
+ */
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getTodo() {
+        return todoService.getTodos();
+    }
+}
